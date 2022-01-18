b5cf9af0e1bf6e3df521f53410a907f7a3d0bcc5
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-01-18 12:25:54 +0100

    Add files via upload

---
+++ Datetime Picker/Datetime Picker/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Datetime_Picker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpDatum = new System.Windows.Forms.DateTimePicker();
+            this.dtpVrijeme = new System.Windows.Forms.DateTimePicker();
+            this.txtIspis = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRezervacija = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtpDatum
+            // 
+            this.dtpDatum.Location = new System.Drawing.Point(251, 41);
+            this.dtpDatum.Name = "dtpDatum";
+            this.dtpDatum.Size = new System.Drawing.Size(200, 20);
+            this.dtpDatum.TabIndex = 0;
+            // 
+            // dtpVrijeme
+            // 
+            this.dtpVrijeme.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpVrijeme.Location = new System.Drawing.Point(251, 100);
+            this.dtpVrijeme.Name = "dtpVrijeme";
+            this.dtpVrijeme.ShowUpDown = true;
+            this.dtpVrijeme.Size = new System.Drawing.Size(200, 20);
+            this.dtpVrijeme.TabIndex = 1;
+            // 
+            // txtIspis
+            // 
+            this.txtIspis.Location = new System.Drawing.Point(64, 164);
+            this.txtIspis.Multiline = true;
+            this.txtIspis.Name = "txtIspis";
+            this.txtIspis.Size = new System.Drawing.Size(387, 149);
+            this.txtIspis.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Upišite datum leta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Upišite vrijeme polaska:";
+            // 
+            // btnRezervacija
+            // 
+            this.btnRezervacija.Location = new System.Drawing.Point(214, 327);
+            this.btnRezervacija.Name = "btnRezervacija";
+            this.btnRezervacija.Size = new System.Drawing.Size(75, 23);
+            this.btnRezervacija.TabIndex = 5;
+            this.btnRezervacija.Text = "Rezervacija";
+            this.btnRezervacija.UseVisualStyleBackColor = true;
+            this.btnRezervacija.Click += new System.EventHandler(this.btnRezervacija_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 362);
+            this.Controls.Add(this.btnRezervacija);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIspis);
+            this.Controls.Add(this.dtpVrijeme);
+            this.Controls.Add(this.dtpDatum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpDatum;
+        private System.Windows.Forms.DateTimePicker dtpVrijeme;
+        private System.Windows.Forms.TextBox txtIspis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRezervacija;
+    }
+}
+
---
+++ Datetime Picker/Datetime Picker/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datetime_Picker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRezervacija_Click(object sender, EventArgs e)
+        {
+            txtIspis.Clear();
+
+            DateTime datum = dtpDatum.Value;
+            DateTime Vrijeme = dtpVrijeme.Value;
+
+            StringBuilder ispis = new StringBuilder();
+            ispis.Append("Vaš datum leta je: ");
+            ispis.Append(datum.ToShortDateString()+"\n");
+            ispis.Append("Vaše vrijeme polaska je: ");
+            ispis.Append(Vrijeme.ToShortTimeString());
+
+            string ispis2 = "Vaš datum leta je "
+                + datum.ToShortDateString()
+                + "\n" + "Vaše vrijeme polaska je: "
+                + Vrijeme.ToShortTimeString();
+
+            txtIspis.Text = ispis.ToString();
+            txtIspis.AppendText (Environment.NewLine + ispis2);
+
+
+        }
+    }
+}
